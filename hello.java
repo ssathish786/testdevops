45b6169f41440436b1449ce7d27bf2369a9ba436
ssathish786 <ssathish786@gmail.com>
2020-02-14 03:35:51 +0530

    yes

--- hello.java
+++ hello.java
@@ -1,11 +1,6 @@
-class Simple{  
+class hello{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
     }  
 }  
-
-class Simple1{  
-    public static void main1(String args[]){  
-     System.out.println("Hello Java1");  
-    }  
-}  
+  
